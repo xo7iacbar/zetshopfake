2885f7ecac107d92bd4a5c6b50ff0707ccac45b5
MAAB <orzubekjumaniyozov2010@gmail.com>
2025-06-23 19:50:25 +0500

    1238988989889

--- app/src/main/java/com/example/zetshop/GooglemapFragment.kt
+++ app/src/main/java/com/example/zetshop/GooglemapFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class GooglemapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
--- app/src/main/java/com/example/zetshop/SaqlanganActivity.kt
+++ app/src/main/java/com/example/zetshop/SaqlanganActivity.kt
@@ -1,20 +1,47 @@
 package com.example.zetshop
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.zetshop.databinding.ActivitySaqlanganBinding
 
 class SaqlanganActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySaqlanganBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_saqlangan)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+      binding=ActivitySaqlanganBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.navButton.setOnItemSelectedListener {
+            when(it.itemId) {
+
+
+                R.id.menuBir ->
+                    {
+supportFragmentManager.beginTransaction().replace(R.id.fragmentcontiner,
+    SavsaqlanganFragment()).commit()
+
+                        return@setOnItemSelectedListener true
+
+                }
+
+
+                R.id.menuIkki ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentcontiner,
+                        SavsaqlanmaganFragment()).commit()
+
+                    return@setOnItemSelectedListener true
+
+                }
+                else ->{
+
+                    return@setOnItemSelectedListener true
+                }
+            }
         }
-    }
+        }
 }
